c2a2e327d1e003cae8899abc757e86c7831d865b
Isabella <collantemendezisabella@gmail>
2023-09-16 06:26:02 -0500

    feat(Mostrar gastos en pantalla)

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const Gasto = () => {
+const Gasto = ({ gasto }) => {
+  const { nombre, cantidad, categoria, fecha } = gasto;
   return (
-    <div>
-        <p>Desde Gastos.jsx</p>
+    <div className="gasto sombra">
+      <div className="contenido-gasto">
+        <div className="descripcion-gasto">
+          <p className="categoria">{categoria}</p>
+          <p className="nombre-gasto">{nombre}</p>
+          <p className="categoria">{cantidad}</p>
+          <p className="categoria">{fecha}</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gasto
+export default Gasto;
